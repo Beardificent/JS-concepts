4da37bdf92bfde07bba7ccc1adc0a0e96574edb1
Beardificent <cis.magito@gmail.com>
2020-12-15 11:34:32 +0100

    added an example of an xhr post request

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+### XHR GET REQUEST
 <img src="https://raw.githubusercontent.com/Beardificent/JavaScript--XHR-GET-request/master/xhr-get-request.png"/>
+
+### XHR POST REQUEST (AJAX)
+<img src="https://raw.githubusercontent.com/Beardificent/JavaScript--XHR-GET-request/master/xhr-post-request.png"/>
--- main.js
+++ main.js
@@ -18,3 +18,20 @@ xhr.onreadystatechange = () => {
     xhr.send();
 }
 
+
+
+/* AJAX POST request using an XMLHttpRequest object.*/
+
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint';
+/*JSON.stringify() will convert a value to a JSON string. By converting the value to a string, we can then send the data to a server.  */
+const data = JSON.stringify({id: '200'});
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE){
+        return xhr.response;
+    }
+}
+
+xhr.open('POST', url);
+xhr.send(data);
